4854c75ed1f708047eef6bf4ecf895d3e666015f
sobaidali <obaidullah_syed@hotmail.com>
2021-06-29 16:35:21 +0500

    constructor function

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,18 @@
 console.log('Hello word!');
+//Constructor function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const another = new Circle(1);
+//new operator
+//creates an empty object
+//then it sets 'this' to point to that object 
+//by default 'this' points to the global object
+//therefore, two ways to create object = factory function or constructor
+
+//if we return an object in a function we refers it as factory function
+//if we use 'this' keyword along with the new operator it refers to as a constructor
