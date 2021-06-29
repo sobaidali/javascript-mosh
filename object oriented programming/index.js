3e67cba1a894987013554070bc65ab714522b81f
sobaidali <obaidullah_syed@hotmail.com>
2021-06-29 16:01:10 +0500

    object literals

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,14 @@
 console.log('Hello word!');
+//objects - collection of key value pairs
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
