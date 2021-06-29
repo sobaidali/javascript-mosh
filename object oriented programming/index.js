12eee20aec41c7d487ecc8ab6829b9abb5001b09
sobaidali <obaidullah_syed@hotmail.com>
2021-06-29 16:11:11 +0500

    factoty functions

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,14 @@
 console.log('Hello word!');
+//If object has one or more methods - we say that object has behaviour
+
+//Factory function
+function createCirlcle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        },
+    }
+};
+
+const circle = createCirlcle(1);
