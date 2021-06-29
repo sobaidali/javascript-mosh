7c156d0b2f453ba4ace014c48f81305f848b1d58
sobaidali <obaidullah_syed@hotmail.com>
2021-06-30 00:41:30 +0500

    functions are objects

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,12 @@
 console.log('Hello word!');
+//In javasript functions are objects
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+Circle.call({}, 1);
+//this function is exactly like
+const another = new Circle(1);
