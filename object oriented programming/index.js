0b7cc439cddb16e1bac1947552361f2cf2196f51
sobaidali <obaidullah_syed@hotmail.com>
2021-06-30 17:15:01 +0500

    exercise

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,32 @@
 console.log('Hello word!');
+class StopWatch {
+    constructor() {
+        let start = false;
+        let startTime = null;
+        let endTime = null;
+
+        this.start = function () {
+            if (start)
+                throw new Error('Stopwatch has already started');
+            start = true;
+            startTime = new Date().getSeconds();
+        };
+        this.stop = function () {
+            if (!start)
+                throw new Error('Stopwatch is not started');
+            start = false;
+            endTime = new Date().getSeconds();
+        };
+        this.reset = function () {
+            startTime = null;
+            endTime = null;
+        };
+        Object.defineProperty(this, 'duration', {
+            get: function () {
+                return endTime - startTime;
+            }
+        });
+    }
+}
+
+const sw = new StopWatch();
