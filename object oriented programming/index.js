c6f36c8d4139fb5ba100d5a620bf7b2fd0c51742
sobaidali <obaidullah_syed@hotmail.com>
2021-06-30 16:39:53 +0500

    getters and setters

--- object oriented programming/index.js
+++ object oriented programming/index.js
@@ -1,1 +1,29 @@
 console.log('Hello word!');
+//Objects in javacript are dynamic
+function Circle(radius) {
+    this.radius = radius;
+
+    let defaultLocation = { x: 0, y: 0 };
+    this.getDefaultLocation = function() {
+        return defaultLocation;
+    }
+
+    this.draw = function() {
+        console.log('draw');
+    }
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if (!value.x || !value.y)
+                throw new Error('Invalid location')
+            defaultLocation = value;
+        }
+    })
+}
+
+const circle = new Circle(1);
+circle.defaultLocation = 1 //Invalid location
+circle.draw(1);
